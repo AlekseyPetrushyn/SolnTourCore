6dc9d804ca0a9bf3cb7286c43890add6e7ed067d
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-03 00:48:29 +0300

    add class library SolnTourCore.Business to solution? and delete class(1).cs file

---
+++ SolnTourCore.Business/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SolnTourCore.Business")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("638b78ef-d960-4eed-a356-a014dbae41cd")]
