60b552d2704ba70e7d219a63167113920186a6ac
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-03 20:56:25 +0300

    add data to test postgresql connection

--- SolnTourCore.Presentation/Controllers/HomeController.cs
+++ SolnTourCore.Presentation/Controllers/HomeController.cs
@@ -3,12 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SolnTourCore.Presentation.Models;
 
 namespace SolnTourCore.Presentation.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly CountryContext _context;
+
+		public HomeController(CountryContext context)
+		{
+			_context = context;
+		}
 		public IActionResult Index()
+		{
+			return View(_context.countries.ToList());
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+
+			return View();
+		}
+
+		public IActionResult Error()
 		{
 			return View();
 		}
