20bf656d58dc0d5e1e2b3d9e4ac8862e8e79248f
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-03 20:54:52 +0300

    add model countries to test postgresql

---
+++ SolnTourCore.Presentation/Models/Countries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace SolnTourCore.Presentation.Models
+{
+	public class country
+	{
+		[Key]
+		public int country_id { get; set; }
+		public string country_name { get; set; }
+		
+	}
+
+	public class CountryContext : DbContext
+	{
+		public CountryContext(DbContextOptions<CountryContext> options) : base(options)
+		{
+			
+		}
+		public DbSet<country> countries { get; set; }
+	}
+}
