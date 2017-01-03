d5c3838c45bf75a457248bb78f24b4b29220458a
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-03 20:55:25 +0300

    add web app structure from web app template

--- SolnTourCore.Presentation/Startup.cs
+++ SolnTourCore.Presentation/Startup.cs
@@ -5,35 +5,63 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SolnTourCore.Presentation.Models;
 
 namespace SolnTourCore.Presentation
 {
 	public class Startup
 	{
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+					.SetBasePath(env.ContentRootPath)
+					.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+					.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+					.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
+
+		public IConfigurationRoot Configuration { get; }
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvc();		//добавляем mvc как сервис для его использования
+			services.AddMvc();    //добавляем mvc как сервис для его использования
+			var connectionString = Configuration["DbContextSetting:ConnectionString"];
+			services.AddDbContext<CountryContext>(
+				opts => opts.UseNpgsql(connectionString)
+			);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
-			loggerFactory.AddConsole();
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
 
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
+				app.UseBrowserLink();
 			}
-			app.UseMvc(routes =>			//создаём маршрут для сопоставления запросов с контроллерами и их методами
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+
+			app.UseStaticFiles();
+
+			app.UseMvc(routes =>
 			{
 				routes.MapRoute(
-					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
+									name: "default",
+									template: "{controller=Home}/{action=Index}/{id?}");
 			});
+
 		}
 	}
 }
