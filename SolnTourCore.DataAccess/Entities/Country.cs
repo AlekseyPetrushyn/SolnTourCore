816ae349acc1e7cc6e38531ed2ca7acb20c84d49
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-13 16:18:43 +0300

    add some changes into project.json files for using attributes

--- SolnTourCore.DataAccess/Entities/Country.cs
+++ SolnTourCore.DataAccess/Entities/Country.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace SolnTourCore.DataAccess.Entities
 {
 	public class Country
 	{
-		public int country_id { get; set; }
-		public string country_name { get; set; }
+		[Key]
+		[Column("country_id")]
+		public int CountryId { get; set; }
+		[Column("country_name")]
+		public string CountryName { get; set; }
 	}
 }
--- SolnTourCore.Presentation/Models/Countries.cs
+++ SolnTourCore.Presentation/Models/Countries.cs
@@ -5,14 +5,17 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SolnTourCore.Presentation.Models
 {
 	public class Country
 	{
 		[Key]
-		public int country_id { get; set; }
-		public string country_name { get; set; }
+		[Column("country_id")]
+		public int CountryId { get; set; }
+		[Column("country_name")]
+		public string CountryName { get; set; }
 		
 	}
 
