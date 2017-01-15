710c49a0c99ebce27d450fb487759f6e476c992c
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-15 22:06:22 +0300

    add class TourContext and add DbSet countries into it

---
+++ SolnTourCore.DataAccess/EFContext/TourContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SolnTourCore.DataAccess.Entities;
+
+namespace SolnTourCore.DataAccess.EFContext
+{
+	public class TourContext : DbContext
+	{
+		public TourContext(DbContextOptions<TourContext> options) : base(options)
+		{
+			
+		}
+
+		public DbSet<Country> countries { get; set; }
+	}
+}
