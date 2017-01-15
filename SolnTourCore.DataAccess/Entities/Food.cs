8b5c7b7beeed108334c824da8d73a0e4673e932e
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-15 22:50:32 +0300

    add Food entity

---
+++ SolnTourCore.DataAccess/Entities/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.DataAccess.Entities
+{
+	public class Food
+	{
+		[Key, Column("food_id")]
+		public int FoodId { get; set; }
+		[Column("food_name")]
+		public string FoodName { get; set; }
+		[Column("description")]
+		public string Description { get; set; }
+	}
+}
