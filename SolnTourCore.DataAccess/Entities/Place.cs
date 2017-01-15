9e0105ba83c331ce15fb61fb4aa906ae8e3998b6
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-15 22:15:02 +0300

    add entity Place

---
+++ SolnTourCore.DataAccess/Entities/Place.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SolnTourCore.DataAccess.Entities
+{
+	public class Place
+	{
+		[Key, Column("place_id")]
+		public int PlaceId { get; set; }
+		[Column("place_name")]
+		public string PlaceName { get; set; }
+		[ForeignKey("CountryId"), Column("country_id")]
+		public int CountryId { get; set; }
+	}
+}
