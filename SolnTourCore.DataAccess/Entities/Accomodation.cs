9a193f8499b3877a017056c521efdbffa72ee62b
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-15 22:25:27 +0300

    add Accomodation entity

---
+++ SolnTourCore.DataAccess/Entities/Accomodation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.DataAccess.Entities
+{
+	public class Accomodation
+	{
+		[Key, Column("accomodation_id")]
+		public int AccomodationId { get; set; }
+		[Column("accomodation_name")]
+		public string AccomodationName { get; set; }
+		[Column("description")]
+		public string Description { get; set; }
+	}
+}
