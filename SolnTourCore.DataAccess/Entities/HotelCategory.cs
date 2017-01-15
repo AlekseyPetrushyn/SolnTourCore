74866754f91e1dadcd0cfb9fe088ac5d343fe173
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-15 22:34:09 +0300

    add HotelCategory entity

---
+++ SolnTourCore.DataAccess/Entities/HotelCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.DataAccess.Entities
+{
+	public class HotelCategory
+	{
+		[Key, Column("hotel_category_id")]
+		public int HotelCategoryId { get; set; }
+		[Column("hotel_category_name")]
+		public string HotelCategoryName { get; set; }
+		[Column("description")]
+		public string Description { get; set; }
+	}
+}
