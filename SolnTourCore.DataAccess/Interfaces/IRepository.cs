e62e3e5640c6c68f351c3d574c771a478f1dcaae
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-20 21:46:24 +0300

    add "public" access modifier to IRepository.cs

--- SolnTourCore.DataAccess/Interfaces/IRepository.cs
+++ SolnTourCore.DataAccess/Interfaces/IRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace SolnTourCore.DataAccess.Interfaces
 {
-	interface IRepository<T> where T : class
+	public interface IRepository<T> where T : class
 	{
 		IEnumerable<T> GetAll();
 		T Get(int id);
