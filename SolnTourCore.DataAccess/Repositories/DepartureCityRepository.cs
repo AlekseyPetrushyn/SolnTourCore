25e9a8220c87dcd65023e3fed38118782b3f3b64
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-22 22:50:41 +0300

    add implementation DepartureCityRepository in DataAccess

---
+++ SolnTourCore.DataAccess/Repositories/DepartureCityRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SolnTourCore.DataAccess.EFContext;
+using SolnTourCore.DataAccess.Entities;
+using SolnTourCore.DataAccess.Interfaces;
+
+namespace SolnTourCore.DataAccess.Repositories
+{
+	public class DepartureCityRepository : IRepository<DepartureCity>
+	{
+		private TourContext _context;
+
+		public DepartureCityRepository(TourContext _context)
+		{
+			this._context = _context;
+		}
+
+		public IEnumerable<DepartureCity> GetAll()
+		{
+			return _context.departure_citys.Include(c => c.Country);
+		}
+
+		public DepartureCity Get(int id)
+		{
+			return _context.departure_citys.Find(id);
+		}
+
+		public IEnumerable<DepartureCity> Find(Func<DepartureCity, bool> predicate)
+		{
+			return _context.departure_citys.Where(predicate).ToList();
+		}
+
+		public void Create(DepartureCity item)
+		{
+			_context.departure_citys.Add(item);
+		}
+
+		public void Update(DepartureCity item)
+		{
+			_context.Entry(item).State = EntityState.Modified;
+		}
+
+		public void Delete(int id)
+		{
+			DepartureCity item = _context.departure_citys.Find(id);
+			if (item != null)
+				_context.departure_citys.Remove(item);
+		}
+	}
+}
