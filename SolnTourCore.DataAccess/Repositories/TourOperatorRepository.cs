cc8c75b0daddb178e716f4006329f37bc230e888
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-23 13:08:41 +0300

    implement TourOperatorRepository in DataAccess layer

---
+++ SolnTourCore.DataAccess/Repositories/TourOperatorRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SolnTourCore.DataAccess.EFContext;
+using SolnTourCore.DataAccess.Entities;
+using SolnTourCore.DataAccess.Interfaces;
+
+namespace SolnTourCore.DataAccess.Repositories
+{
+	public class TourOperatorRepository : IRepository<TourOperator>
+	{
+		private TourContext _context;
+
+		public TourOperatorRepository(TourContext _context)
+		{
+			this._context = _context;
+		}
+
+		public IEnumerable<TourOperator> GetAll()
+		{
+			return _context.tour_operators.Include(o => o.Transfer)
+				.Include(o => o.AdditionalService);
+		}
+
+		public TourOperator Get(int id)
+		{
+			return _context.tour_operators.Find(id);
+		}
+
+		public IEnumerable<TourOperator> Find(Func<TourOperator, bool> predicate)
+		{
+			return _context.tour_operators.Where(predicate).ToList();
+		}
+
+		public void Create(TourOperator item)
+		{
+			_context.tour_operators.Add(item);
+		}
+
+		public void Update(TourOperator item)
+		{
+			_context.Entry(item).State = EntityState.Modified;
+		}
+
+		public void Delete(int id)
+		{
+			TourOperator item = _context.tour_operators.Find(id);
+			if (item != null)
+				_context.tour_operators.Remove(item);
+		}
+	}
+}
