d24efc39a955240f996f94e50c5e6925730260a3
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-24 23:34:31 +0300

    add DTO entity FoodDTO

---
+++ SolnTourCore.Business/DTO/FoodDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class FoodDTO
+	{
+		public int FoodId { get; set; }
+		public string FoodName { get; set; }
+		public string Description { get; set; }
+	}
+}
