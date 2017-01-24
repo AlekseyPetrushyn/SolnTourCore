14c17286481006284622939abc6f3c3e1a6e2f51
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-24 23:31:18 +0300

    add DTO entity HotelCategoryDTO

---
+++ SolnTourCore.Business/DTO/HotelCategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class HotelCategoryDTO
+	{
+		public int HotelCategoryId { get; set; }
+		public string HotelCategoryName { get; set; }
+		public string Description { get; set; }
+	}
+}
