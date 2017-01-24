7ae4b5498f514f6f8aed5a5a0f9a4e848fe0b8db
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-25 00:01:01 +0300

    add DTO entity TourOperatorDTO

---
+++ SolnTourCore.Business/DTO/TourOperatorDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class TourOperatorDTO
+	{
+		public int OperatorId { get; set; }
+		public string OperatorName { get; set; }
+		public AdditionalServiceDTO AdditionalService { get; set; }
+		public int AdditionalServiceId { get; set; }
+		public TransferDTO Transfer { get; set; }
+		public int TransferId { get; set; }
+	}
+}
