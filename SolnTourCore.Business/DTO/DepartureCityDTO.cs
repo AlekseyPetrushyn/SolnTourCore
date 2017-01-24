a30f3635f9216d922b62ea10519d0e0ef253b598
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-24 23:49:03 +0300

    add DTO entity DepartureCityDTO

---
+++ SolnTourCore.Business/DTO/DepartureCityDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class DepartureCityDTO
+	{
+		public int CityId { get; set; }
+		public CountryDTO Country { get; set; }
+		public int CountryId { get; set; }
+		public string CityName { get; set; }
+	}
+}
