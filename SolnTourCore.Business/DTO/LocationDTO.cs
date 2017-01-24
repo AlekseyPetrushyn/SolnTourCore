d3c3ca18188d83a63ce6915f9180cc9efbde12d4
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-24 23:35:56 +0300

    add new DTO entity LocationDTO

---
+++ SolnTourCore.Business/DTO/LocationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class LocationDTO
+	{
+		public int LocationId { get; set; }
+		public string LocationName { get; set; }
+		public string Description { get; set; }
+	}
+}
