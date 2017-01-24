6476daa3fa2fe593360d2a8da457600e0416595e
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-24 23:30:00 +0300

    add DTO entity AccomodationDTO

---
+++ SolnTourCore.Business/DTO/AccomodationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class AccomodationDTO
+	{
+		public int AccomodationId { get; set; }
+		public string AccomodationName { get; set; }
+		public string Description { get; set; }
+	}
+}
