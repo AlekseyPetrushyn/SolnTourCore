482288f88f82574db3a94fdcabfa1cded8889bcb
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-24 23:37:23 +0300

    add DTO entity RecreationDTO

---
+++ SolnTourCore.Business/DTO/RecreationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class RecreationDTO
+	{
+		public int RecreationId { get; set; }
+		public string RecreationName { get; set; }
+		public string Description { get; set; }
+	}
+}
