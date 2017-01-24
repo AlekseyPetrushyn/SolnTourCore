571da675038130dbbb69711140f6d2e5d9fcf2ce
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-24 23:47:27 +0300

    add DTO entity TransportDTO

---
+++ SolnTourCore.Business/DTO/TransportDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class TransportDTO
+	{
+		public int TransportId { get; set; }
+		public string TransportName { get; set; }
+	}
+}
