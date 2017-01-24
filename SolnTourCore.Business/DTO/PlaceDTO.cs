d0a1ee5f331157fc0b7949f41aff9c524a89db27
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-24 23:28:10 +0300

    add DTO entity PlaceDTO

---
+++ SolnTourCore.Business/DTO/PlaceDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class PlaceDTO
+	{
+		public int PlaceId { get; set; }
+		public string PlaceName { get; set; }
+		public CountryDTO Country { get; set; }
+		public int CountryId { get; set; }
+	}
+}
