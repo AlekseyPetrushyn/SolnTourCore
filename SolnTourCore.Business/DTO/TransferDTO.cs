2b7dadb9d0d583d303d7e0ff7a7ff6548aeb9ce1
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-24 23:53:52 +0300

    add DTO entity TransferDTO

---
+++ SolnTourCore.Business/DTO/TransferDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class TransferDTO
+	{
+		public int TransferId { get; set; }
+		public TransportDTO Transport { get; set; }
+		public int TransportId { get; set; }
+		public DepartureCityDTO DepartureCity { get; set; }
+		public int DepartureCityId { get; set; }
+		public DestinationCityDTO DestinationCity { get; set; }
+		public int DestinationCityId { get; set; }
+		public decimal Price { get; set; }
+	}
+}
