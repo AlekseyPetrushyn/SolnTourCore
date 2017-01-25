aa95c80606f8e771fa420f99132cf5928ecc6b57
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-25 13:35:04 +0300

    change controller for mapping test

--- SolnTourCore.Presentation/Controllers/HomeController.cs
+++ SolnTourCore.Presentation/Controllers/HomeController.cs
@@ -2,27 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SolnTourCore.Business.DTO;
+using SolnTourCore.Business.Services.Interfaces.ServiceInterfaces;
 using SolnTourCore.DataAccess.EFContext;
 using SolnTourCore.DataAccess.Entities;
 using SolnTourCore.DataAccess.Interfaces;
+using SolnTourCore.Presentation.ViewModels;
 
 
 namespace SolnTourCore.Presentation.Controllers
 {
 	public class HomeController : Controller
 	{
+		private ICountryService _countryService;
+
 		private IRepository<Hotel> _hotelRepository;
 
 
-		public HomeController(IRepository<Hotel> hotelRepository)
+		public HomeController(IRepository<Hotel> hotelRepository, ICountryService countryService)
 		{
+			_countryService = countryService;
 			_hotelRepository = hotelRepository;
 		}
 		public IActionResult Index()
 		{
-			return View(_hotelRepository.GetAll());
+			IEnumerable<CountryDTO> item = _countryService.GetAll();
+			AutoMapper.Mapper.Initialize(cfg => cfg.CreateMap<CountryDTO, CountryViewModel>());
+			var country = AutoMapper.Mapper.Map<IEnumerable<CountryDTO>, List<CountryViewModel>>(item);
+			return View(country);
 		}
 
 		public IActionResult About()
