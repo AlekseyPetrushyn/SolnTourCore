20ff9be647dda184ea4f26b1c94b473e2beb9577
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-25 10:16:31 +0300

    add ITourService interface for implementation business services

---
+++ SolnTourCore.Business/Services/Interfaces/ITourService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.Services.Interfaces
+{
+	public interface ITourService<T> where T : class 
+	{
+		IEnumerable<T> GetAll();
+		T Get(int id);
+		void Create(T item);
+		void Update(T item);
+		void Delete(int id);
+		void Dispose();
+	}
+}
