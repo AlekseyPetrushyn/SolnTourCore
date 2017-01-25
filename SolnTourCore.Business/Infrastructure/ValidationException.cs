93b67846a587716f04a2d52c16b10f864de609b2
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-25 09:36:44 +0300

    add ValidationException class for data validation on Business layer and transfer them to Presentation layer

---
+++ SolnTourCore.Business/Infrastructure/ValidationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.Infrastructure
+{
+	public class ValidationException : Exception
+	{
+			//валидация данных на бизнес уровне
+			//валидация с передачей информации на уровень представления
+		public string Property { get; protected set; }
+		public ValidationException(string message, string property) : base(message)
+		{
+			Property = property;
+		}
+	}
+}
