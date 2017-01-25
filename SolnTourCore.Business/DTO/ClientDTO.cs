0fb0359bb6f5b990bbe6f630cc8d1bc82f59d3fb
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-25 08:23:50 +0300

    add DTO entity ClientDTO in DTO folder Business layer

---
+++ SolnTourCore.Business/DTO/ClientDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class ClientDTO
+	{
+		public int ClientId { get; set; }
+		public string SecondName { get; set; }
+		public string FirstName { get; set; }
+		public string Patronymic { get; set; }
+		public DateTime BirthDay { get; set; }
+		public string Address { get; set; }
+		public string MobilNumber { get; set; }
+		public string Email { get; set; }
+		public DiscountDTO Discount { get; set; }
+		public int DiscountId { get; set; }
+		public string Login { get; set; }
+		public string Password { get; set; }
+	}
+}
