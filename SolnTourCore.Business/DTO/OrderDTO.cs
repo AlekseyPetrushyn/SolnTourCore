377845b704c2bcaad8d0976e0d9480bed7329fbd
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-25 08:34:50 +0300

    add DTO entity OrderDTO

---
+++ SolnTourCore.Business/DTO/OrderDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class OrderDTO
+	{
+		public int OrderId { get; set; }
+		public TourDTO Tour { get; set; }
+		public int TourId { get; set; }
+		public ClientDTO Client { get; set; }
+		public int ClientId { get; set; }
+		public EmployeeDTO Employee { get; set; }
+		public int EmployeeId { get; set; }
+	}
+}
