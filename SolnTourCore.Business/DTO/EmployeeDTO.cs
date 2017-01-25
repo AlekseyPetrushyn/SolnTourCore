8df1b25b1cf4b62997ca737af7c6425816134fc0
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-25 08:30:45 +0300

    add DTO entity EmployeeDTO

---
+++ SolnTourCore.Business/DTO/EmployeeDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class EmployeeDTO
+	{
+		public int EmployeeId { get; set; }
+		public string SecondName { get; set; }
+		public string FirstName { get; set; }
+		public string Patronymic { get; set; }
+		public DateTime BirthDay { get; set; }
+		public string Address { get; set; }
+		public string MobilNumber { get; set; }
+		public string WorkNumber { get; set; }
+		public AccessLevelDTO AccessLevel { get; set; }
+		public int AccessId { get; set; }
+		public string Login { get; set; }
+		public string Password { get; set; }
+	}
+}
