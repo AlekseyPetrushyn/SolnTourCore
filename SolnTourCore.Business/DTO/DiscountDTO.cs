cb335c48c3e405671205369efdcd3b6c8ca791f1
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-25 08:18:20 +0300

    add DTO entity DiscountDTO into DTO folder Business layer

---
+++ SolnTourCore.Business/DTO/DiscountDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class DiscountDTO
+	{
+		public int DiscountId { get; set; }
+		public string DiscountName { get; set; }
+		public double Percent { get; set; }
+	}
+}
