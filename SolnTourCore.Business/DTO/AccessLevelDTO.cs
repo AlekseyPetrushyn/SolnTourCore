51f85dbfc58896d3d9a1cddce4740d6c81a9f2e4
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-25 08:25:40 +0300

    add DTO entity AccessLevelDTO

---
+++ SolnTourCore.Business/DTO/AccessLevelDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Business.DTO
+{
+	public class AccessLevelDTO
+	{
+		public int AccessId { get; set; }
+		public string AccessName { get; set; }
+		public string Description { get; set; }
+	}
+}
