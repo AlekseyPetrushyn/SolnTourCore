312440bfc7538092d2f90a1e124888e69adf3f60
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-27 09:05:23 +0300

    add Countryervice and implement void method Create

---
+++ SolnTourCore.Business/Services/Implementations/CountryService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SolnTourCore.Business.DTO;
+using SolnTourCore.Business.Services.Interfaces.ServiceInterfaces;
+using SolnTourCore.DataAccess.Entities;
+using SolnTourCore.DataAccess.Interfaces;
+
+namespace SolnTourCore.Business.Services.Implementations
+{
+	public class CountryService : ICountryService
+	{
+		private IRepository<Country> _countryRepository;
+
+		public CountryService(IRepository<Country> countryRepository)
+		{
+			_countryRepository = countryRepository;
+		}
+		
+		public IEnumerable<CountryDTO> GetAll()
+		{
+			return AutoMapper.Mapper.Map<IEnumerable<Country>, List<CountryDTO>>(_countryRepository.GetAll());
+		}
+
+		public CountryDTO Get(int id)
+		{
+			return AutoMapper.Mapper.Map<Country, CountryDTO>(_countryRepository.Get(id));
+		}
+
+		public void Create(CountryDTO item)
+		{
+			Country country = new Country
+			{
+				CountryId = _countryRepository.GetAll().Count() + 1,
+				CountryName = item.CountryName
+			};
+			_countryRepository.Create(country);
+		}
+
+		public void Update(CountryDTO item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
