fb266fc105dd53ae5580c63f1e6ac6218cf2f65b
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-28 00:14:53 +0300

    add GetAll() method

--- SolnTourCore.Business/Services/Implementations/PlaceService.cs
+++ SolnTourCore.Business/Services/Implementations/PlaceService.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using SolnTourCore.Business.DTO;
 using SolnTourCore.Business.Services.Interfaces.ServiceInterfaces;
+using SolnTourCore.DataAccess.Entities;
+using SolnTourCore.DataAccess.Interfaces;
 
 namespace SolnTourCore.Business.Services.Implementations
 {
     public class PlaceService : IPlaceService
     {
+        private IRepository<Place> _placeRepository;
+
+        public PlaceService(IRepository<Place> placeRepository)
+        {
+            _placeRepository = placeRepository;
+        }
         public IEnumerable<PlaceDTO> GetAll()
         {
-            throw new NotImplementedException();
+            return AutoMapper.Mapper.Map<IEnumerable<Place>, List<PlaceDTO>>(_placeRepository.GetAll());
         }
 
         public PlaceDTO Get(int id)
