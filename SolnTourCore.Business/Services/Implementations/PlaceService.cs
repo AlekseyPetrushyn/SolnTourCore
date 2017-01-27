635a82122a4dec5ea99d24f2c4f35f5fe78eab84
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-28 00:12:00 +0300

    add new PlaceService class without code

---
+++ SolnTourCore.Business/Services/Implementations/PlaceService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SolnTourCore.Business.DTO;
+using SolnTourCore.Business.Services.Interfaces.ServiceInterfaces;
+
+namespace SolnTourCore.Business.Services.Implementations
+{
+    public class PlaceService : IPlaceService
+    {
+        public IEnumerable<PlaceDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlaceDTO Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(PlaceDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(PlaceDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
