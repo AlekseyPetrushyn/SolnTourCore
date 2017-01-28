43a10e53f12613dcfe50dc87b6c85414034128fb
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-28 23:14:49 +0300

    add PlaceViewModel into Presentation layer

---
+++ SolnTourCore.Presentation/ViewModels/PlaceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class PlaceViewModel
+    {
+        public int PlaceId { get; set; }
+        public string PlaceName { get; set; }
+        public CountryViewModel Country { get; set; }
+        public int CountryId { get; set; }
+    }
+}
