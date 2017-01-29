74a9e7db77f03511b8610c11da5dd2178db8a97d
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-29 10:55:38 +0300

    change parent interface

---
+++ SolnTourCore.Business/Services/Interfaces/ServiceInterfaces/IHotelCategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SolnTourCore.Business.DTO;
+using SolnTourCore.DataAccess.Interfaces;
+
+namespace SolnTourCore.Business.Services.Interfaces.ServiceInterfaces
+{
+    public interface IHotelCategoryService : ITourService<HotelCategoryDTO>
+    {
+
+    }
+}
