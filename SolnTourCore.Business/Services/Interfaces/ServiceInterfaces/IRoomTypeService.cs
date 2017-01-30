61af47d4ea9d6902cc30589a7404983ad1bc11ad
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 13:54:35 +0300

    add IRoomTypeService interface

---
+++ SolnTourCore.Business/Services/Interfaces/ServiceInterfaces/IRoomTypeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SolnTourCore.Business.DTO;
+
+namespace SolnTourCore.Business.Services.Interfaces.ServiceInterfaces
+{
+    public interface IRoomTypeService : ITourService<RoomTypeDTO>
+    {
+
+    }
+}
