81acec014cd61ad94ccfd824d241731f59a5982b
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-31 00:15:20 +0300

    add DiscountViewModel into Presentation layer

---
+++ SolnTourCore.Presentation/ViewModels/DiscountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class DiscountViewModel
+    {
+        public int DiscountId { get; set; }
+        public string DiscountName { get; set; }
+        public double Percent { get; set; }
+    }
+}
