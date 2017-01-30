a88951f299bf39c2165f9adff8d489c8ffff0020
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 18:14:05 +0300

    add FoodViewModel into Presentation layer

---
+++ SolnTourCore.Presentation/ViewModels/FoodViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class FoodViewModel
+    {
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public string Description { get; set; }
+    }
+}
