0e7360a9e0cd79f78979c6473ba56de58b2486b5
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 13:54:51 +0300

    add RoomTypeService class

---
+++ SolnTourCore.Business/Services/Implementations/RoomTypeService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SolnTourCore.Business.DTO;
+using SolnTourCore.Business.Services.Interfaces.ServiceInterfaces;
+using SolnTourCore.DataAccess.Entities;
+using SolnTourCore.DataAccess.Interfaces;
+
+namespace SolnTourCore.Business.Services.Implementations
+{
+    public class RoomTypeService : IRoomTypeService
+    {
+        private IRepository<RoomType> _roomTypeRepository;
+
+        public RoomTypeService(IRepository<RoomType> roomTypeRepository)
+        {
+            _roomTypeRepository = roomTypeRepository;
+        }
+        public IEnumerable<RoomTypeDTO> GetAll()
+        {
+            return AutoMapper.Mapper.Map<IEnumerable<RoomType>, List<RoomTypeDTO>>(_roomTypeRepository.GetAll());
+        }
+
+        public RoomTypeDTO Get(int id)
+        {
+            return AutoMapper.Mapper.Map<RoomType, RoomTypeDTO>(_roomTypeRepository.Get(id));
+        }
+
+        public void Create(RoomTypeDTO item)
+        {
+            RoomType roomType = new RoomType
+            {
+                RoomTypeId = _roomTypeRepository.GetAll().Count() + 1,
+                RoomTypeName = item.RoomTypeName,
+                Description = item.Description
+            };
+            _roomTypeRepository.Create(roomType);
+        }
+
+        public void Update(RoomTypeDTO item)
+        {
+            RoomType roomType = _roomTypeRepository.Get(item.RoomTypeId);
+            roomType.RoomTypeName = item.RoomTypeName;
+            roomType.Description = item.Description;
+            _roomTypeRepository.Update(roomType);
+        }
+
+        public void Delete(int id)
+        {
+            _roomTypeRepository.Delete(id);
+        }
+    }
+}
