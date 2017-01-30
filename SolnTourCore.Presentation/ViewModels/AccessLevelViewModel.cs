60fbd61eb46916abc675472ba07e2fd715e725cf
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-31 00:32:05 +0300

    add AccessLevelViewModel

---
+++ SolnTourCore.Presentation/ViewModels/AccessLevelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class AccessLevelViewModel
+    {
+		public int AccessId { get; set; }
+		public string AccessName { get; set; }
+		public string Description { get; set; }
+    }
+}
