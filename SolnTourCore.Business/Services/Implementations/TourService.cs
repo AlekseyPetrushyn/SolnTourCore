43215407f1ddbbeb8f24bf81bade4805badc2384
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 23:06:44 +0300

    add TourService into Bsiness layer

---
+++ SolnTourCore.Business/Services/Implementations/TourService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SolnTourCore.Business.DTO;
+using SolnTourCore.Business.Services.Interfaces.ServiceInterfaces;
+using SolnTourCore.DataAccess.Entities;
+using SolnTourCore.DataAccess.Interfaces;
+
+namespace SolnTourCore.Business.Services.Implementations
+{
+    public class TourService : ITourService
+    {
+        private IRepository<Tour> _repository { get; set; }
+
+        public TourService(IRepository<Tour> repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<TourDTO> GetAll()
+        {
+            return AutoMapper.Mapper.Map<IEnumerable<Tour>, List<TourDTO>>(_repository.GetAll());
+        }
+
+        public TourDTO Get(int id)
+        {
+            return AutoMapper.Mapper.Map<Tour, TourDTO>(_repository.Get(id));
+        }
+
+        public void Create(TourDTO item)
+        {
+            Tour tour = new Tour
+            {
+                TourId = _repository.GetAll().Count() + 1,
+                TourOperatorId = item.TourOperatorId,
+                HotelId = item.HotelId,
+                DestinationDate = item.DestinationDate,
+                DepartureDate = item.DepartureDate
+            };
+            _repository.Create(tour);
+        }
+
+        public void Update(TourDTO item)
+        {
+            var tour = _repository.Get(item.TourId);
+            tour.TourOperatorId = item.TourOperatorId;
+            tour.HotelId = item.HotelId;
+            tour.DepartureDate = item.DepartureDate;
+            tour.DestinationDate = item.DestinationDate;
+            _repository.Update(tour);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
