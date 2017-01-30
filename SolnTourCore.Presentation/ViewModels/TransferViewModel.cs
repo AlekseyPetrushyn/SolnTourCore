4911cd2b5e2897772859b481f5a0ad944ae0cd66
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 22:32:29 +0300

    add TransferViewModel into Presentation layer

---
+++ SolnTourCore.Presentation/ViewModels/TransferViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class TransferViewModel
+    {
+        public int TransferId { get; set; }
+        public int TransportId { get; set; }
+        public int DepartureCityId { get; set; }
+        public int DestinationCityId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
