d30cc275eb8eb89bad33ea5b152980bd28a29628
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 21:40:37 +0300

    add TransportViewModel into Presentation layer

---
+++ SolnTourCore.Presentation/ViewModels/TransportViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class TransportViewModel
+    {
+        public int TransportId { get; set; }
+        public string TransportName { get; set; }
+    }
+}
