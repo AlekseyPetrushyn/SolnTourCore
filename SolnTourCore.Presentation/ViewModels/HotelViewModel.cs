ecd88ead859fe16da0a213253824bf42e70fdf14
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 20:00:43 +0300

    add HotelViewModel into Presentation

---
+++ SolnTourCore.Presentation/ViewModels/HotelViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class HotelViewModel
+    {
+        public int HotelId { get; set; }
+        public int PlaceId { get; set; }
+        public string HotelName { get; set; }
+        public int HotelCategoryId { get; set; }
+        public int FoodId { get; set; }
+        public int RoomTypeId { get; set; }
+        public int AccomodationId { get; set; }
+        public int LocationId { get; set; }
+        public int RecreationId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
