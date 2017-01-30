1120e811c0ac5398f11642b7076023ac5f26b723
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 13:57:39 +0300

    add RoomTypeViewModel into Presentation layer

---
+++ SolnTourCore.Presentation/ViewModels/RoomTypeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class RoomTypeViewModel
+    {
+        public int RoomTypeId { get; set; }
+        public string RoomTypeName { get; set; }
+        public string Description { get; set; }
+    }
+}
