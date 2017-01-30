f520903abfa6778f26ab22239d067a14d11dcd38
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-31 00:50:08 +0300

    add OrderViewModel

---
+++ SolnTourCore.Presentation/ViewModels/OrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class OrderViewModel
+    {
+		public int OrderId { get; set; }
+		public int TourId { get; set; }
+		public int ClientId { get; set; }
+		public int EmployeeId { get; set; }
+		public decimal OrderPrice { get; set; }
+    }
+}
