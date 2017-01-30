ecc1fe5c75d7f88fd2daf5d52fe566b31d50ec35
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 22:54:32 +0300

    add TourOperatoViewModel

---
+++ SolnTourCore.Presentation/ViewModels/TourOperatorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class TourOperatorViewModel
+    {
+        public int OperatorId { get; set; }
+        public string OperatorName { get; set; }
+        public int AdditionalServiceId { get; set; }
+        public int TransferId { get; set; }
+    }
+}
