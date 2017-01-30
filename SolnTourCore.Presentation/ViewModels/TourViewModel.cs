de0e25b467341c0b33fcd3b188f9bfdd8f984f9b
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 23:07:51 +0300

    add TourViewModel into Presentation layer

---
+++ SolnTourCore.Presentation/ViewModels/TourViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class TourViewModel
+    {
+        public int TourId { get; set; }
+        public int TourOperatorId { get; set; }
+        public int HotelId { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime DestinationDate { get; set; }
+    }
+}
