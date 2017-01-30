b22b3425ef32be76c7348699a433d3d9c88dcc34
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-31 00:31:37 +0300

    add AccessLevelService

---
+++ SolnTourCore.Business/Services/Implementations/AccessLevelservice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SolnTourCore.Business.DTO;
+using SolnTourCore.Business.Services.Interfaces.ServiceInterfaces;
+using SolnTourCore.DataAccess.Entities;
+using SolnTourCore.DataAccess.Interfaces;
+
+namespace SolnTourCore.Business.Services.Implementations
+{
+    public class AccessLevelService : IAccessLevelService
+    {
+        private IRepository<AccessLevel> _repository { get; set; }
+
+        public AccessLevelService(IRepository<AccessLevel> repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<AccessLevelDTO> GetAll()
+        {
+            return AutoMapper.Mapper.Map<IEnumerable<AccessLevel>, List<AccessLevelDTO>>(_repository.GetAll());
+        }
+
+        public AccessLevelDTO Get(int id)
+        {
+            return AutoMapper.Mapper.Map<AccessLevel, AccessLevelDTO>(_repository.Get(id));
+        }
+
+        public void Create(AccessLevelDTO item)
+        {
+            AccessLevel level = new AccessLevel
+            {
+                AccessId = _repository.GetAll().Count() + 1,
+                AccessName = item.AccessName,
+                Description = item.Description
+            };
+            _repository.Create(level);
+        }
+
+        public void Update(AccessLevelDTO item)
+        {
+            var level = _repository.Get(item.AccessId);
+            level.AccessName = item.AccessName;
+            level.Description = item.Description;
+            _repository.Update(level);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
