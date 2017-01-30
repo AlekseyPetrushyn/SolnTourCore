b6e097d4e81716634be8700c8b3229761acae31d
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-01-30 22:21:13 +0300

    add DestinationCityViewModel into Presentation layer

---
+++ SolnTourCore.Presentation/ViewModels/DestinationCityViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolnTourCore.Presentation.ViewModels
+{
+    public class DestinationCityViewModel
+    {
+        public int CityId { get; set; }
+        public int CountryId { get; set; }
+        public string CityName { get; set; }
+    }
+}
