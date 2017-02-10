83b70c16facd6a2a22ab6877a9b47151cecf8b8a
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-02-10 13:38:29 +0300

    add pblic modificer

--- SolnTourCore.Business/Services/Interfaces/ServiceInterfaces/IDiscountService.cs
+++ SolnTourCore.Business/Services/Interfaces/ServiceInterfaces/IDiscountService.cs
@@ -6,7 +6,7 @@ using SolnTourCore.Business.DTO;
 
 namespace SolnTourCore.Business.Services.Interfaces.ServiceInterfaces
 {
-    interface IDiscountService : IService<DiscountDTO>
+    public interface IDiscountService : IService<DiscountDTO>
     {
 
     }
