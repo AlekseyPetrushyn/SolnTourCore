19414bd1efe4bb29c14fee5cd24c38ecb6bde982
AlekseyPetrushyn <aliakseypiatrushyn@gmail.com>
2017-02-10 13:36:25 +0300

    add public modificer

--- SolnTourCore.Business/Services/Interfaces/ServiceInterfaces/ITourOperatorService.cs
+++ SolnTourCore.Business/Services/Interfaces/ServiceInterfaces/ITourOperatorService.cs
@@ -6,7 +6,7 @@ using SolnTourCore.Business.DTO;
 
 namespace SolnTourCore.Business.Services.Interfaces.ServiceInterfaces
 {
-    interface ITourOperatorService : IService<TourOperatorDTO>
+    public interface ITourOperatorService : IService<TourOperatorDTO>
     {
 
     }
